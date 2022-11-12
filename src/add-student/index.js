e998bc2f2a56840b4f2cfd41bf9134002ab146fb
aryaraj132 <aryaraj132@gmail.com>
2022-11-12 23:22:24 +0530

    add-student action

---
+++ src/add-student/index.js
@@ -0,0 +1,32 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+(async () => {
+  try {
+    const githubToken = core.getInput('github-token', { required: true });
+    const filePath = core.getInput('file-path');
+    const fileContent = core.getInput('file-content');
+
+    // add a comment to the issue or pull request
+    // @TODO: with a markdown sheild / badge
+    const client = github.getOctokit(githubToken);
+    const context = github.context;
+
+    if (context.payload.action !== 'opened') {
+      console.log('No issue / pull request was opened, skipping');
+      return;
+    }
+
+    if (!!context.payload.issue) {
+      // await client.issues.createComment({
+      //   owner: context.issue.owner,
+      //   repo: context.issue.repo,
+      //   issue_number: context.issue.number,
+      //   body: issueMessage + footerTags
+      // });
+      console.log('Issue Body, ', context.payload.issue.body);
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+})();
